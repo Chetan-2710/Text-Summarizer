3f46375464f4716ba88339a90e3eba69a41aa360
Chetan <chetankolakanti27@gmail.com>
2025-03-16 23:34:16 +0530

    Done till modular Data Ingestion stage

---
+++ src/TextSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+# All the constants like paths of the files and folders are present in this file
+
+from pathlib import Path
+CONFIG_FILE_PATH=Path("config/config.yaml")
+PARAMS_FILE_PATH=Path("params.yaml")
